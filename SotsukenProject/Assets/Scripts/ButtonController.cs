567faf53c659f713c4abb427fcd72d26a8766bed
KazumsaHayashi <kazumasaice1215@icloud.com>
2019-06-18 04:47:43 +0900

    Scene管理,Audio関連(Recordはモーションと音声を非同期処理する必要ありか)

---
+++ SotsukenProject/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class ButtonController : MonoBehaviour
+    {
+
+        #region Public Properties
+
+        #endregion
+
+        #region Private Properties
+
+        #endregion
+
+        #region Public Variables
+
+        #endregion
+
+        #region Private Variables
+
+        #endregion
+
+        #region Photon Messages
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods	
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+
+        public void GotoRehearsal()
+        {
+            SceneManager.LoadScene("RehearsalScene");
+        }
+
+        public void GotoRehearsalStarter()
+        {
+            SceneManager.LoadScene("RehearsalStarterScene");
+        }
+
+        public void GotoPlayback()
+        {
+            SceneManager.LoadScene("PlaybackScene");
+        }
+
+        #endregion
+    }
+}
+
