d77eba52cd09c1ec5ca42d384d7c6df0652a216b
KazumsaHayashi <kazumasaice1215@icloud.com>
2019-07-10 18:45:32 +0900

    カメラの設定とRecorderの設定

---
+++ SotsukenProject/Assets/Scripts/MovieCapture.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System;
+//using UnityEngine.UI;
+//sing UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class MovieCapture : MonoBehaviour
+    {
+
+        #region Public Properties
+
+        #endregion
+
+        #region Private Properties
+
+        #endregion
+
+        #region Public Variables
+
+        #endregion
+
+        #region Private Variables
+
+        #endregion
+
+        #region Photon Messages
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods	
+
+        void Awake()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+
+        #endregion
+    }
+}
